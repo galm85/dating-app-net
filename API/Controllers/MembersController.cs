4b0b5491ae2f2f17c4406ad820a611b04a5b6a8a
Gal <galm85@gmail.com>
2026-09-08 14:24:10 +0300

    backend - create authentication with token

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,9 +1,11 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
+
 
 public class MembersController(AppDbContext context) : BaseApiController
 {
@@ -14,7 +16,7 @@ public class MembersController(AppDbContext context) : BaseApiController
         return members;
     }
 
-
+    [Authorize]
     [HttpGet("{id}")] // localhost:5001/api/members/gal-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
